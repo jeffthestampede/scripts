babc1108e98a628b4ec25d9250e14dec097a9337
jeffthestampede <jeffthestampede@gmail.com>
2014-08-19 17:22:45 -0700

    strip whitespace from vividseats scraper

--- vivid.rb
+++ vivid.rb
@@ -8,7 +8,6 @@ schedule = Nokogiri::HTML(c)
 CSV.open("lakers.csv", "a+") do |csv|
 
   schedule.css("div.productionsDay").zip(schedule.css("div.productionsDate"), schedule.css("div.productionsTime"), schedule.css("div.productionsEvent")).each do |day, date, time, event|
-    csv << [day.text, date.text, time.text, event.text]
+    csv << [day.text.strip, date.text.strip, time.text.strip, event.text.strip]
   end
-
 end
